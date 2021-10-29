0db647dd71f03fffe30739332c2eea5f3b4526bc
Poul Henriksen <pohe@easj.dk>
2021-10-29 11:46:01 +0200

    Menu added

--- BookCatalog/InsertCodeHere.cs
+++ BookCatalog/InsertCodeHere.cs
@@ -4,68 +4,67 @@ namespace BookCatalog
 {
     class InsertCodeHere
     {
-        // public int ShowMenu()
-        // {
-        //     //
-        // }
+        
 
         public void MyCode()
         {
             // The FIRST line of code should be BELOW this line
 
+            Menu menu = new Menu();
+            menu.Run();
 
-            BookCatalogList bookCatalogList = new BookCatalogList();
-            
+            //BookCatalogList bookCatalogList = new BookCatalogList();
+
             //Test af AddBook
-            Book firstBook = new Book("121212", "C# programming", "John Sharp", 597);
-            Console.WriteLine($"Antal bøger inden add af bog {bookCatalogList.Count  }");
-            bookCatalogList.AddBook(firstBook);
-            Book secondBook = new Book("131313", "Pascal programming", "John Pascal", 444);
-            Book thirdBook = new Book("141414", "Python programming", "John Python", 888);
-            bookCatalogList.AddBook(secondBook);
-            bookCatalogList.AddBook(thirdBook);
-            Console.WriteLine($"Antal bøger efter add af 3 bøger {bookCatalogList.Count  }");
+            //Book firstBook = new Book("121212", "C# programming", "John Sharp", 597);
+            //Console.WriteLine($"Antal bøger inden add af bog {bookCatalogList.Count  }");
+            //bookCatalogList.AddBook(firstBook);
+            //Book secondBook = new Book("131313", "Pascal programming", "John Pascal", 444);
+            //Book thirdBook = new Book("141414", "Python programming", "John Python", 888);
+            //bookCatalogList.AddBook(secondBook);
+            //bookCatalogList.AddBook(thirdBook);
+            //Console.WriteLine($"Antal bøger efter add af 3 bøger {bookCatalogList.Count  }");
 
-            //Test af LookUpBook
-            Book foundBook = bookCatalogList.LookupBook("131313");
-            if (foundBook != null)
-            {
-                //Udskriv foundBook
-                Console.WriteLine($"Bog fundet { foundBook.ToString()}");
-            }
-            else
-            {
-                //Udskriv bog ikke fundet
-                Console.WriteLine("Bog ikke fundet");
-            }
+            ////Test af LookUpBook
+            //Book foundBook = bookCatalogList.LookupBook("131313");
+            //if (foundBook != null)
+            //{
+            //    //Udskriv foundBook
+            //    Console.WriteLine($"Bog fundet { foundBook.ToString()}");
+            //}
+            //else
+            //{
+            //    //Udskriv bog ikke fundet
+            //    Console.WriteLine("Bog ikke fundet");
+            //}
 
-            //Test af DeleteBook
-            //Console.WriteLine($"Antal bøger inden delete af bog {bookCatalogList.Count  }");
-            //bookCatalogList.DeleteBook("131313");
-            //Console.WriteLine($"Antal bøger efter delete af bog {bookCatalogList.Count  }");
+            ////Test af DeleteBook
+            ////Console.WriteLine($"Antal bøger inden delete af bog {bookCatalogList.Count  }");
+            ////bookCatalogList.DeleteBook("131313");
+            ////Console.WriteLine($"Antal bøger efter delete af bog {bookCatalogList.Count  }");
 
-            Console.WriteLine("Print books");
-            bookCatalogList.PrintBooks();
-            Book upDatedBook = new Book("131313", "C# 2.0 programming", "John Sharp v2", 450);
-            bookCatalogList.UpdateBook("131313", upDatedBook);
-            Console.WriteLine("Print books after update");
-            bookCatalogList.PrintBooks();
+            //Console.WriteLine("Print books");
+            //bookCatalogList.PrintBooks();
+            //Book upDatedBook = new Book("131313", "C# 2.0 programming", "John Sharp v2", 450);
+            //bookCatalogList.UpdateBook("131313", upDatedBook);
+            //Console.WriteLine("Print books after update");
+            //bookCatalogList.PrintBooks();
 
-            Console.WriteLine("Test of BookCatalogDictionary");
+            //Console.WriteLine("Test of BookCatalogDictionary");
 
-            BookCatalogDictionary bookCatalogDictionary = new BookCatalogDictionary();
-            bookCatalogDictionary.AddBook(firstBook);
-            bookCatalogDictionary.AddBook(secondBook);
-            bookCatalogDictionary.AddBook(thirdBook);
-            bookCatalogDictionary.PrintBooks();
+            //BookCatalogDictionary bookCatalogDictionary = new BookCatalogDictionary();
+            //bookCatalogDictionary.AddBook(firstBook);
+            //bookCatalogDictionary.AddBook(secondBook);
+            //bookCatalogDictionary.AddBook(thirdBook);
+            //bookCatalogDictionary.PrintBooks();
 
-            
-            Book newBook = new Book("131313", "bla bla bla", "John bla", 45455);
-            bookCatalogDictionary.AddBook(newBook);
-            bookCatalogDictionary.DeleteBook("141414");
 
-            Console.WriteLine("Books after add(with existing isbn and delete af book with isbn 141414");
-            bookCatalogDictionary.PrintBooks();
+            //Book newBook = new Book("131313", "bla bla bla", "John bla", 45455);
+            //bookCatalogDictionary.AddBook(newBook);
+            //bookCatalogDictionary.DeleteBook("141414");
+
+            //Console.WriteLine("Books after add(with existing isbn and delete af book with isbn 141414");
+            //bookCatalogDictionary.PrintBooks();
             // The LAST line of code should be ABOVE this line
         }
     }
---
+++ BookCatalog/Menu.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookCatalog
+{
+    public class Menu
+    {
+
+        public Menu()
+        {
+            
+        }
+
+        public int ShowMenu()
+        {
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("1. Tilføj bog til BookCatalogList");
+            Console.WriteLine("2. Fjern bog bog fra BookCatalogList");
+            Console.WriteLine("3. Opdater bog fra BookCatalogList");
+            Console.WriteLine("4. Tilføj bog til BookCatalogDictionary");
+            Console.WriteLine("5. Fjern bog bog fra BookCatalogDictionary");
+            Console.WriteLine("6. Opdater bog fra BookCatalogDictionary");
+            Console.WriteLine("Tast 0 for afslut");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Indtast dit valg:");
+            string choice = Console.ReadLine();
+            int input = -1;
+            if (int.TryParse(choice, out input))
+            {
+                return input;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public void Run()
+        {
+            int valg = ShowMenu();
+            while (valg != 0)
+            {
+                switch (valg)
+                {
+                    case 1: 
+                        Console.Clear();
+                        //Metode der indlæser og håndterer add Bog til BookCatalogList
+                        AddBookToList();
+                        break;
+                    case 2: // todo
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Fejl i input");
+                        break;
+                }
+
+                Console.Clear();
+                valg = ShowMenu();
+
+            }
+
+        }
+
+        private void AddBookToList()
+        {
+            Console.WriteLine("Angiv ISBN");
+            string isbn = Console.ReadLine();
+            Console.WriteLine("Angiv titel");
+            string title = Console.ReadLine();
+            Console.WriteLine("Angiv author");
+            string author = Console.ReadLine();
+            Console.WriteLine("Angiv antal sider");
+            int noOfPages = int.Parse(Console.ReadLine());
+
+            Book b = new Book(isbn, title, author, noOfPages);
+
+
+        }
+    }
+}
